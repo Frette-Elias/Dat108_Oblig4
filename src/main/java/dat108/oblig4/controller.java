3a489ef41b96ec2251987aa6e64f502eb2e24328
h571569 <571569@stud.hvl.no>
2025-10-20 14:28:49 +0200

    first

---
+++ src/main/java/dat108/oblig4/controller.java
@@ -0,0 +1,22 @@
+package dat108.oblig4;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class controller {
+
+    @GetMapping("/deltagerliste")
+    public String sjekk() {
+        return "deltagerliste";
+    }
+    @GetMapping("/")
+    public String sjekk1() {
+        return "paameldt";
+    }
+    @GetMapping("/2")
+    public String sjekk2() {
+        return "paamelding_med_melding";
+    }
+}
+
