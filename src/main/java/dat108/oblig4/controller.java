8051400414d451d2bfd6cf444d6978c1f146a050
h184906 <elias.frette@gmail.com>
2025-10-23 18:31:25 +0200

    Kombinert med endring av side

--- src/main/java/dat108/oblig4/controller.java
+++ src/main/java/dat108/oblig4/controller.java
@@ -1,25 +1,30 @@
 package dat108.oblig4;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class controller {
 
     @GetMapping("/deltagerliste")
-    public String sjekk() {
+    public String deltagerliste() {
         return "deltagerliste";
     }
-    @GetMapping("/")
-    public String sjekk1() {
+    @GetMapping("/paamelding_med_melding")
+    public String paamelding_med_melding() {
         return "paamelding_med_melding";
     }
-    @GetMapping("/2")
-    public String sjekk2() {
+    @GetMapping("/paameldt")
+    public String paameldt() {
         return "paameldt";
     }
-
-
-
-
+    @PostMapping("/paamelding_med_melding")
+    public String registrerDeltager(@RequestParam String fornavn, @RequestParam String etternavn, Model model,
+                                    @RequestParam String mobil, @RequestParam String passord,
+                                    @RequestParam String bekreftPassord, @RequestParam String kjonn) {
+        return "redirect:/paameldt";
+    }
 }
